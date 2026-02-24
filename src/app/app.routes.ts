5f4ade3386ad6ed375b08993b62409cff88a821c
Tesi <alessandrostuccio@gmail.com>
2026-02-24 10:55:36 +0100

    Implementazione home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { RegisterComponent } from './pages/register/register';
 import { DashboardComponent } from './pages/dashboard/dashboard'; // 1. AGGIUNTO L'IMPORT
+import { HomeComponent } from './pages/home/home';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent }, // 2. AGGIUNTA LA ROTTA
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
